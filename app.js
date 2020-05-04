18e0190a2a3e7fd698791642a8610529e351900a
Shubham Gupta <shubham15gupta09@gmail.com>
2020-05-04 23:38:15 +0530

    may_4 build with working famework

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+var app = express();
+const fs = require("fs");
+var upload = require("express-fileupload");
+
+app.use(upload()); // configure middleware
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/routes/index.html");
+});
+function clean_up(){
+  const path = '/images/image.jpg';
+  fs.unlink(path , ()=>{});
+}
+app.post("/upload", function (req, res) {
+  console.log(req.files);
+  if (req.files.upfile) {
+    var file = req.files.upfile,
+      name = "image.jpg";
+    var uploadpath = __dirname + "/images/" + name;
+    file.mv(uploadpath, function (err) {
+      console.log("File Uploaded", name);
+      res.sendFile(__dirname+"/routes/mobilenet.html");
+      clean_up();
+    });
+  }
+});
+app.get("/model.json" , function(req,res){
+  console.log(req.files);
+  res.sendFile(__dirname + "/tf_modelfile/model.json");
+});
+
+app.get("/group1-shard1of1.bin" , function(req,res){
+  console.log(req.files);
+  res.sendFile(__dirname + "/tf_modelfile/group1-shard1of1.bin");
+});
+
+app.get("/images/image.jpg",(req,res)=>{
+  res.sendFile(__dirname+"/images/image.jpg")
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT , (req,res)=>{
+  console.log("server started on port : 3000");
+});
